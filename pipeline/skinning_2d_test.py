23aacb9d6a23b285f44e27e0df2b4bfb1ba388c9
Gregory Kramida <algomorph@gmail.com>
2021-04-12 11:43:26 -0400

    Add 3D DQB example

--- pipeline/skinning_2d_test.py
+++ pipeline/skinning_2d_test.py
@@ -15,10 +15,10 @@ def lbs(ws, tfs):
 if __name__ == "__main__":
     # 2 links simulation
 
-    link_lens = [1.0, 1.0]
+    link_lengths = [1.0, 1.0]
     links = [dualquat(quat.identity()),
              dualquat(quat.identity(),
-                      np.array([link_lens[0], 0.0, 0.0]))]
+                      np.array([link_lengths[0], 0.0, 0.0]))]
 
     n_surface = 20
     surfaces1 = np.r_[[np.linspace(0.0, 2.0, n_surface),
@@ -28,10 +28,12 @@ if __name__ == "__main__":
                        -0.2 * np.ones(n_surface),
                        np.zeros(n_surface)]].T
     surfaces = np.concatenate([surfaces1, surfaces2], axis=0)
+    print(surfaces)
 
-    w_coeff = 2.0
-    w_tmp = np.array([max(0.0, min(1.0, -w_coeff * (i / n_surface - 0.5) + 0.5)) for i in range(n_surface)])
+    weight_coefficient = 2.0
+    w_tmp = np.array([max(0.0, min(1.0, -weight_coefficient * (i / n_surface - 0.5) + 0.5)) for i in range(n_surface)])
     weights = np.tile(np.r_[[w_tmp, 1.0 - w_tmp]].T, (2, 1))
+    print(weights)
 
     import matplotlib.pyplot as plt
     import matplotlib.animation as animation
@@ -41,15 +43,18 @@ if __name__ == "__main__":
 
     def update_anim(i):
         plt.cla()
-        th = i * 0.5
-        trans = dualquat(quat(*tf.quaternion_from_euler(0.0, 0.0, np.deg2rad(th))))
-        t_links_dq = [links[0], links[1] * trans * links[1].inverse()]
-        t_links_tf = [l.to_matrix() for l in t_links_dq]
-        t_surfaces1 = np.array([op.dlb(w, t_links_dq).transform_point(s) for w, s in zip(weights, surfaces)])
-        t_surfaces2 = np.array([np.dot(lbs(w, t_links_tf), np.r_[s, 1.0])[:3] for w, s in zip(weights, surfaces)])
+        theta = i * 0.5
+        transform = dualquat(quat(*tf.quaternion_from_euler(0.0, 0.0, np.deg2rad(theta))))
+        transformed_links_dual_quaternions = [links[0], links[1] * transform * links[1].inverse()]
+        # for comparison, also compute matrix transformations
+        transformed_links_matrices = [link.to_matrix() for link in transformed_links_dual_quaternions]
+        # apply dual quaternions directly
+        t_surfaces1 = np.array([op.dlb(w, transformed_links_dual_quaternions).transform_point(s) for w, s in zip(weights, surfaces)])
+        # apply matrix transformations
+        t_surfaces2 = np.array([np.dot(lbs(w, transformed_links_matrices), np.r_[s, 1.0])[:3] for w, s in zip(weights, surfaces)])
 
-        plt.plot([0.0, link_lens[0], link_lens[1] * np.cos(np.deg2rad(th)) + link_lens[0]],
-                 [0.0, 0.0, link_lens[1] * np.sin(np.deg2rad(th))], 'b-')
+        plt.plot([0.0, link_lengths[0], link_lengths[1] * np.cos(np.deg2rad(theta)) + link_lengths[0]],
+                 [0.0, 0.0, link_lengths[1] * np.sin(np.deg2rad(theta))], 'b-')
         plt.plot(t_surfaces2[:, 0], t_surfaces2[:, 1], 'go', label="LBS")
         plt.plot(t_surfaces1[:, 0], t_surfaces1[:, 1], 'ro', label="DLB")
         plt.xlim(-0.2, 2.2)
--- pipeline/skinning_3d_test.py
+++ pipeline/skinning_3d_test.py
@@ -9,6 +9,8 @@
 
 from __future__ import division
 import sys
+import math
+from typing import List
 
 from scipy.spatial.transform import Rotation
 import numpy as np
@@ -22,17 +24,78 @@ from dq3d import op
 PROGRAM_EXIT_SUCCESS = 0
 
 
-def main():
+def compute_anchor_weight_python(anchor_to_point_distance, node_coverage):
+    return math.exp(-(anchor_to_point_distance * anchor_to_point_distance) / (2 * node_coverage * node_coverage))
 
-    mesh = o3d.geometry.TriangleMesh.create_cylinder(0.5, 5.0, 20, 20)
-    mesh.compute_vertex_normals()
-    rotation: Rotation = Rotation.from_euler('x', 90, degrees=True)
-    mesh.rotate(rotation.as_matrix())
-    o3d.visualization.draw_geometries([mesh],
-                                      front=[0, 0, 1],
-                                      lookat=[0, 0, 1.5],
-                                      up=[0, 1.0, 0],
-                                      zoom=0.7)
+
+class Viewer:
+    def __init__(self):
+        cross_section_count = 20
+        cylinder_height = 5.0
+        node_count = 3
+        node_coverage = 1.2
+
+        node_y_coordinates = np.linspace(0.0, cylinder_height, node_count)
+        self.edge_lengths = [node_y_coordinates[i_node + 1] - node_y_coordinates[i_node] for i_node in range(len(node_y_coordinates) - 1)]
+        self.node_locations = np.array([[0.0, y, 0.0] for y in node_y_coordinates])
+
+        self.node_dual_quaternions = [dualquat(quat.identity())]
+        for edge_length in self.edge_lengths:
+            self.node_dual_quaternions.append(dualquat(quat.identity(), [0.0, edge_length, 0.0]))
+        # for y_coordinate in node_y_coordinates:
+        #     self.node_dual_quaternions.append(dualquat(quat.identity(), [0.0, y_coordinate, 0.0]))
+
+        self.cylinder_mesh = o3d.geometry.TriangleMesh.create_cylinder(0.5, 5.0, 20, cross_section_count)
+        self.cylinder_mesh.compute_vertex_normals()
+        initial_rotation: Rotation = Rotation.from_euler('x', 90, degrees=True)
+
+        self.cylinder_mesh.rotate(initial_rotation.as_matrix())
+        self.cylinder_mesh.translate(np.array([0.0, 2.5, 0.0]))
+        vertices = np.array(self.cylinder_mesh.vertices)
+
+        self.mesh_coordinate_frame = o3d.geometry.TriangleMesh.create_coordinate_frame(
+            size=0.6, origin=[-2, 0, 0])
+
+        vertex_to_anchor_vectors = np.dstack([vertices - node_location for node_location in self.node_locations])
+        vertex_to_anchor_square_distances = np.sum(vertex_to_anchor_vectors ** 2, axis=1)
+        self.weights = np.exp(-(vertex_to_anchor_square_distances / (2 * node_coverage * node_coverage)))
+        self.vertices = vertices
+
+        self.i_frame = 0
+        self.angle_increment = 0.25
+
+    def run_visualizer(self):
+        def deform(visualizer):
+            self.i_frame += 1
+            if self.i_frame > 90 / (self.angle_increment * (len(self.node_dual_quaternions) - 1)):
+                return
+            node_angles_degrees = [self.i_frame * self.angle_increment * i_node for i_node in range(1, len(self.node_dual_quaternions))]
+
+            # transformations from frame 0 to this frame
+            node_rotations = [dualquat(quat(*tf.quaternion_from_euler(0.0, 0.0, np.deg2rad(angle)))) for angle in node_angles_degrees]
+
+            transformed_nodes_dual_quaternions: List[dualquat] = [self.node_dual_quaternions[0]]
+            for i_node in range(1, len(self.node_dual_quaternions)):
+                original_dq = self.node_dual_quaternions[i_node]
+                transformed_dq: dualquat = original_dq * node_rotations[i_node - 1] * original_dq.inverse()
+                transformed_nodes_dual_quaternions.append(transformed_dq)
+
+            transformed_vertices = np.array([op.dlb(weights, transformed_nodes_dual_quaternions).transform_point(vertex)
+                                             for weights, vertex in zip(self.weights, self.vertices)])
+            self.cylinder_mesh.vertices = o3d.utility.Vector3dVector(transformed_vertices)
+            self.cylinder_mesh.compute_vertex_normals()
+            visualizer.update_geometry(self.cylinder_mesh)
+
+            return False
+
+        o3d.visualization.draw_geometries_with_animation_callback([self.cylinder_mesh, self.mesh_coordinate_frame],
+                                                                  callback_function=deform,
+                                                                  window_name="DQB Skinning 3D test")
+
+
+def main():
+    viewer = Viewer()
+    viewer.run_visualizer()
     return PROGRAM_EXIT_SUCCESS
 
 
