Fix tensor indexing in anchor computation

tests/test_warp_mesh.py
@@ -26,27 +26,26 @@ def test_compute_anchors_euclidean(device):
     points = np.array(mesh.vertices)
     points_o3d = o3c.Tensor(points, device=device)
 
+    K = 4
+
     nodes_tiled = np.tile(nodes, (len(points), 1, 1))
     points_tiled = np.tile(points, (1, len(nodes))).reshape(len(points), -1, 3)
     distance_matrix = np.linalg.norm(points_tiled - nodes_tiled, axis=2)
-    distance_sorted_node_indices = distance_matrix.argsort(axis=1)
-    vertex_anchors_gt = distance_sorted_node_indices # [:, :4]
+    distance_sorted_node_indices = distance_matrix.argsort(axis=1)  # distances of closest nodes to each point
+    vertex_anchors_gt = np.sort(distance_sorted_node_indices[:, :K], axis=1)
     distances_sorted = np.take_along_axis(distance_matrix, distance_sorted_node_indices, axis=1)
-    distances_gt = distances_sorted # [:, :4]
+    distances_gt = distances_sorted[:, :K]
 
     old_vertex_anchors, old_vertex_weights = nnrt.compute_vertex_anchors_euclidean(nodes, points, 0.5)
     old_vertex_anchors_sorted = np.sort(old_vertex_anchors, axis=1)
-    vertex_anchors, vertex_weights = nnrt.geometry.compute_anchors_and_weights_euclidean(points_o3d, nodes_o3d, 4, 0, 0.5)
+    old_vertex_weights_sorted = np.sort(old_vertex_weights, axis=1)
+    vertex_anchors, vertex_weights = nnrt.geometry.compute_anchors_and_weights_euclidean(points_o3d, nodes_o3d, K, 0, 0.5)
     vertex_anchors_sorted = np.sort(vertex_anchors.cpu().numpy(), axis=1)
+    vertex_weights_sorted = np.sort(vertex_weights.cpu().numpy(), axis=1)
 
-    print(vertex_anchors_gt)
-    print(distances_gt)
-    print(old_vertex_anchors)
-
-    #TODO: establish WTF is going on here.
-
+    assert np.allclose(vertex_anchors_gt, old_vertex_anchors_sorted)
     assert np.allclose(vertex_anchors_sorted, old_vertex_anchors_sorted)
-    assert np.allclose(vertex_weights.cpu().numpy(), old_vertex_weights)
+    assert np.allclose(vertex_weights_sorted, old_vertex_weights_sorted)
 
 
 @pytest.mark.parametrize("device", [o3d.core.Device('cuda:0'), o3d.core.Device('cpu:0')])