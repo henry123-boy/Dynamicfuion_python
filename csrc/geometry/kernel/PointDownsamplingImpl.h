8d5d2a79ec94fc1cf89ab7bc4ee43864cea0b78b
Gregory Kramida <algomorph@gmail.com>
2022-03-10 08:33:36 -0500

    Begin work on cell merging for radius downsampling (WIP)

--- csrc/geometry/kernel/PointDownsamplingImpl.h
+++ csrc/geometry/kernel/PointDownsamplingImpl.h
@@ -86,18 +86,10 @@ struct PointAggregationBin {
 };
 
 
-
-
-} // anonymous namespace
-
 template<open3d::core::Device::DeviceType DeviceType>
-void DownsamplePointsByRadius(o3c::Tensor& downsampled_points, const o3c::Tensor& original_points, float radius) {
-	o3gk::NDArrayIndexer original_point_indexer(original_points, 1);
-	auto original_point_count = original_points.GetLength();
-	auto device = original_points.GetDevice();
-
-	//region GET POINT COLLECTION BOUNDS / EXTREMA
-
+void FindPointCollectionBounds(Eigen::Vector3f& grid_bin_min_bound, Eigen::Vector3f& grid_bin_max_bound,
+                               const o3gk::NDArrayIndexer& original_point_indexer, const o3c::Device& device,
+                               int64_t original_point_count) {
 	NNRT_DECLARE_ATOMIC(float, min_x);
 	NNRT_INITIALIZE_ATOMIC(float, min_x, 0.f);
 	NNRT_DECLARE_ATOMIC(float, min_y);
@@ -114,7 +106,7 @@ void DownsamplePointsByRadius(o3c::Tensor& downsampled_points, const o3c::Tensor
 	o3c::ParallelFor(
 			device, original_point_count,
 			NNRT_LAMBDA_CAPTURE_CLAUSE NNRT_DEVICE_WHEN_CUDACC(int64_t workload_idx) {
-				Eigen::Map<Eigen::Vector3f> point(original_point_indexer.GetDataPtr<float>(workload_idx));
+				const Eigen::Map<Eigen::Vector3f> point(original_point_indexer.GetDataPtr<float>(workload_idx));
 				NNRT_ATOMIC_MIN(min_x, point.x());
 				NNRT_ATOMIC_MIN(min_y, point.y());
 				NNRT_ATOMIC_MIN(min_z, point.z());
@@ -124,35 +116,17 @@ void DownsamplePointsByRadius(o3c::Tensor& downsampled_points, const o3c::Tensor
 			}
 	);
 
-	Eigen::Vector3f grid_bin_min_bound(NNRT_GET_ATOMIC_VALUE_CPU(min_x), NNRT_GET_ATOMIC_VALUE_CPU(min_y), NNRT_GET_ATOMIC_VALUE_CPU(min_z));
-	Eigen::Vector3f grid_bin_max_bound(NNRT_GET_ATOMIC_VALUE_CPU(max_x), NNRT_GET_ATOMIC_VALUE_CPU(max_y), NNRT_GET_ATOMIC_VALUE_CPU(max_z));
+	grid_bin_min_bound = Eigen::Vector3f(NNRT_GET_ATOMIC_VALUE_CPU(min_x), NNRT_GET_ATOMIC_VALUE_CPU(min_y), NNRT_GET_ATOMIC_VALUE_CPU(min_z));
+	grid_bin_max_bound = Eigen::Vector3f(NNRT_GET_ATOMIC_VALUE_CPU(max_x), NNRT_GET_ATOMIC_VALUE_CPU(max_y), NNRT_GET_ATOMIC_VALUE_CPU(max_z));
 
 	NNRT_CLEAN_UP_ATOMIC(min_x);NNRT_CLEAN_UP_ATOMIC(min_y);NNRT_CLEAN_UP_ATOMIC(min_z);NNRT_CLEAN_UP_ATOMIC(max_x);NNRT_CLEAN_UP_ATOMIC(
 			max_y);NNRT_CLEAN_UP_ATOMIC(max_z);
-	//endregion
+}
 
-	// region ESTIMATE BIN COUNT IN DENSE 3D BIN BLOCK & FIND CENTER
-
-	const auto bin_size = radius;
-
-	auto grid_spatial_extents = grid_bin_max_bound - grid_bin_min_bound;
-	auto grid_center = grid_bin_min_bound + (grid_spatial_extents / 2.f);
-
-	auto grid_bin_extents = Eigen::Vector3i(static_cast<int32_t>(std::ceil(grid_spatial_extents.x() / bin_size)),
-	                                        static_cast<int32_t>(std::ceil(grid_spatial_extents.y() / bin_size)),
-	                                        static_cast<int32_t>(std::ceil(grid_spatial_extents.z() / bin_size)));
-
-	// ensure each dimension is divisible by 2, so that later we can process grid directionally with step 2
-	grid_bin_extents.x() = grid_bin_extents.x() + grid_bin_extents.x() % 2;
-	grid_bin_extents.y() = grid_bin_extents.y() + grid_bin_extents.y() % 2;
-	grid_bin_extents.z() = grid_bin_extents.z() + grid_bin_extents.z() % 2;
-
-	const auto bin_count = grid_bin_extents.x() * grid_bin_extents.y() * grid_bin_extents.z();
-	// endregion
-
-	// INITIALIZE BINS
-	o3c::Blob bin_blob(static_cast<int64_t>(sizeof(PointAggregationBin) * bin_count), device);
-	auto bins = reinterpret_cast<PointAggregationBin*>(bin_blob.GetDataPtr());
+template<open3d::core::Device::DeviceType DeviceType>
+void InitializeBins(const o3c::Device& device,
+                    int64_t bin_count,
+                    PointAggregationBin* bins) {
 	o3c::ParallelFor(
 			device, bin_count,
 			NNRT_LAMBDA_CAPTURE_CLAUSE NNRT_DEVICE_WHEN_CUDACC(int64_t workload_idx) {
@@ -163,9 +137,16 @@ void DownsamplePointsByRadius(o3c::Tensor& downsampled_points, const o3c::Tensor
 				bin.count = 0;
 			}
 	);
+}
 
-
-	// COMPUTE POINT AGGREGATES FOR EACH BIN
+template<open3d::core::Device::DeviceType DeviceType>
+void ComputeBinAggregates(const o3gk::NDArrayIndexer& original_point_indexer,
+                          const o3c::Device& device,
+                          int64_t original_point_count,
+                          PointAggregationBin* bins,
+                          const Eigen::Vector3f& grid_bin_min_bound,
+                          const Eigen::Vector3i& grid_bin_extents,
+                          float bin_size) {
 	o3c::ParallelFor(
 			device, original_point_count,
 			NNRT_LAMBDA_CAPTURE_CLAUSE NNRT_DEVICE_WHEN_CUDACC(int64_t workload_idx) {
@@ -187,36 +168,169 @@ void DownsamplePointsByRadius(o3c::Tensor& downsampled_points, const o3c::Tensor
 #endif
 			}
 	);
+}
 
-	// COMPUTE BIN AVERAGES
+template<o3c::Device::DeviceType DeviceType>
+void ComputeBinAverages(int64_t& downsampled_point_count,
+                        o3c::Device& device,
+                        int64_t bin_count,
+                        PointAggregationBin* bins) {
+	NNRT_DECLARE_ATOMIC(uint32_t, downsampled_point_count_atomic);
+	NNRT_INITIALIZE_ATOMIC(uint32_t, downsampled_point_count_atomic, 0);
+
+	// COMPUTE BIN AVERAGES AND COUNT RESULTING POINTS
 	o3c::ParallelFor(
 			device, bin_count,
 			NNRT_LAMBDA_CAPTURE_CLAUSE NNRT_DEVICE_WHEN_CUDACC(int64_t workload_idx) {
 				PointAggregationBin& bin = bins[workload_idx];
 #ifdef __CUDACC__
-				bin.x /= static_cast<float>(bin.count);
-				bin.y /= static_cast<float>(bin.count);
-				bin.z /= static_cast<float>(bin.count);
+				if(bin.count != 0){
+					bin.x /= static_cast<float>(bin.count);
+					bin.y /= static_cast<float>(bin.count);
+					bin.z /= static_cast<float>(bin.count);
+					NNRT_ATOMIC_ADD(downsampled_point_count_atomic, 1L);
+				}
 #else
-				bin.x.store(bin.x.load() / static_cast<float>(bin.count.load()));
-				bin.y.store(bin.y.load() / static_cast<float>(bin.count.load()));
-				bin.z.store(bin.z.load() / static_cast<float>(bin.count.load()));
+				int bin_point_count = bin.count.load();
+				if (bin_point_count != 0) {
+					bin.x.store(bin.x.load() / static_cast<float>(bin_point_count));
+					bin.y.store(bin.y.load() / static_cast<float>(bin_point_count));
+					bin.z.store(bin.z.load() / static_cast<float>(bin_point_count));
+					downsampled_point_count_atomic++;
+				}
 #endif
 			}
 	);
 
+	downsampled_point_count = static_cast<int64_t>(NNRT_GET_ATOMIC_VALUE_CPU(downsampled_point_count_atomic));
+}
+
+template<o3c::Device::DeviceType DeviceType>
+void ComputeBinAverages(o3c::Device& device,
+                        int64_t bin_count,
+                        PointAggregationBin* bins) {
+	int64_t downsampled_point_count;
+	ComputeBinAverages<DeviceType>(downsampled_point_count, device, bin_count, bins);
+}
+
+
+} // anonymous namespace
+
+template<open3d::core::Device::DeviceType DeviceType>
+void DownsamplePointsByRadius(o3c::Tensor& downsampled_points, const o3c::Tensor& original_points, float radius) {
+	o3gk::NDArrayIndexer original_point_indexer(original_points, 1);
+	auto original_point_count = original_points.GetLength();
+	auto device = original_points.GetDevice();
+
+	Eigen::Vector3f grid_bin_min_bound, grid_bin_max_bound;
+	FindPointCollectionBounds<DeviceType>(grid_bin_min_bound, grid_bin_max_bound, original_point_indexer, device, original_point_count);
+
+	// region ESTIMATE BIN COUNT IN DENSE 3D BIN BLOCK & FIND CENTER
+
+	const auto bin_size = radius;
+
+	auto grid_spatial_extents = grid_bin_max_bound - grid_bin_min_bound;
+	auto grid_center = grid_bin_min_bound + (grid_spatial_extents / 2.f);
+
+	auto grid_bin_extents = Eigen::Vector3i(std::max(static_cast<int32_t>(std::ceil(grid_spatial_extents.x() / bin_size)), 1),
+	                                        std::max(static_cast<int32_t>(std::ceil(grid_spatial_extents.y() / bin_size)), 1),
+	                                        std::max(static_cast<int32_t>(std::ceil(grid_spatial_extents.z() / bin_size)), 1));
+
+	// ensure each dimension is divisible by 2, so that later we can process grid directionally with step 2
+	grid_bin_extents.x() = grid_bin_extents.x() + grid_bin_extents.x() % 2;
+	grid_bin_extents.y() = grid_bin_extents.y() + grid_bin_extents.y() % 2;
+	grid_bin_extents.z() = grid_bin_extents.z() + grid_bin_extents.z() % 2;
+
+	const auto bin_count = grid_bin_extents.x() * grid_bin_extents.y() * grid_bin_extents.z();
+	// endregion
+
+
+	o3c::Blob bin_blob(static_cast<int64_t>(sizeof(PointAggregationBin) * bin_count), device);
+	auto bins = reinterpret_cast<PointAggregationBin*>(bin_blob.GetDataPtr());
+	InitializeBins<DeviceType>(device, bin_count, bins);
+
+
+	ComputeBinAggregates<DeviceType>(original_point_indexer, device, original_point_count, bins,
+	                                 grid_bin_min_bound, grid_bin_extents, bin_size);
+
+	ComputeBinAverages<DeviceType>(device, bin_count, bins);
+
 	//TODO  MERGE BINS AS NECESSARY
 
-	// o3c::ParallelFor(
-	// 		device, bin_count / 2,
-	// 		NNRT_LAMBDA_CAPTURE_CLAUSE NNRT_DEVICE_WHEN_CUDACC(int64_t workload_idx) {
-	// 			PointAggregationBin& bin = bins[workload_idx * 2];
-	// 			auto bin_coordinate = UnravelBinLinearIndex<DeviceType>(workload_idx * 2, grid_bin_extents);
-	// 			auto next_bin_coordinate = bin_coordinate + Eigen::Vector3i(1, 0, 0);
-	// 			auto next_bin_index =
-	//
-	// 		}
-	// );
+	o3c::ParallelFor(
+			device, bin_count / 2,
+			NNRT_LAMBDA_CAPTURE_CLAUSE NNRT_DEVICE_WHEN_CUDACC(int64_t workload_idx) {
+				int64_t bin_index = workload_idx * 2;
+				if (bin_index >= bin_count)
+					return;
+				PointAggregationBin& bin = bins[bin_index];
+
+				bool has_point = false;
+				Eigen::Vector3f bin_point;
+				int bin_point_count;
+#ifdef __CUDACC__
+				if(bin.count != 0){
+					has_point = true;
+					bin_point.x() = bin.x;
+					bin_point.y() = bin.y;
+					bin_point.z() = bin.z;
+					bin_point_count = bin.count;
+				}
+#else
+				if (bin.count.load() != 0) {
+					has_point = true;
+					bin_point.x() = bin.x.load();
+					bin_point.y() = bin.y.load();
+					bin_point.z() = bin.z.load();
+					bin_point_count = bin.count.load();
+				}
+#endif
+				if (!has_point)
+					return;
+
+				auto bin_coordinate = UnravelBinLinearIndex<DeviceType>(workload_idx * 2, grid_bin_extents);
+				auto next_bin_coordinate = bin_coordinate + Eigen::Vector3i(1, 0, 0);
+				auto next_bin_index = RavelBinLinearIndex<DeviceType>(next_bin_coordinate, grid_bin_extents);
+				if (next_bin_index >= bin_count)
+					return;
+				PointAggregationBin& next_bin = bins[next_bin_index];
+				Eigen::Vector3f next_bin_point;
+				int next_bin_point_count;
+				next_bin_point_count = next_bin.count;
+#ifdef __CUDACC__
+				if(next_bin.count != 0){
+					has_point = true;
+					next_bin_point.x() = next_bin.x;
+					next_bin_point.y() = next_bin.y;
+					next_bin_point.z() = next_bin.z;
+
+				}
+#else
+				if (next_bin.count.load() != 0) {
+					has_point = true;
+					next_bin_point.x() = next_bin.x.load();
+					next_bin_point.y() = next_bin.y.load();
+					next_bin_point.z() = next_bin.z.load();
+					next_bin_point_count = next_bin.count.load();
+				}
+#endif
+				if (!has_point)
+					return;
+
+				// merge points
+				if ((next_bin_point - bin_point).norm() < radius) {
+					auto average_point =
+							(bin_point * bin_point_count + next_bin_point * next_bin_point_count) / (bin_point_count + next_bin_point_count);
+					auto bin_coordinate = DeterminePointsBinCoordinate<DeviceType>(average_point, grid_bin_min_bound, bin_size);
+					auto merged_bin_index = RavelBinLinearIndex<DeviceType>(bin_coordinate, grid_bin_extents);
+					if(merged_bin_index == bin_index){
+						//FIXME
+					}
+				}
+
+			}
+	);
+
 
 }
 
@@ -226,40 +340,8 @@ void GridDownsamplePoints(open3d::core::Tensor& downsampled_points, const open3d
 	auto original_point_count = original_points.GetLength();
 	auto device = original_points.GetDevice();
 
-	//region GET POINT COLLECTION BOUNDS / EXTREMA
-
-	NNRT_DECLARE_ATOMIC(float, min_x);
-	NNRT_INITIALIZE_ATOMIC(float, min_x, 0.f);
-	NNRT_DECLARE_ATOMIC(float, min_y);
-	NNRT_INITIALIZE_ATOMIC(float, min_y, 0.f);
-	NNRT_DECLARE_ATOMIC(float, min_z);
-	NNRT_INITIALIZE_ATOMIC(float, min_z, 0.f);
-	NNRT_DECLARE_ATOMIC(float, max_x);
-	NNRT_INITIALIZE_ATOMIC(float, max_x, 0.f);
-	NNRT_DECLARE_ATOMIC(float, max_y);
-	NNRT_INITIALIZE_ATOMIC(float, max_y, 0.f);
-	NNRT_DECLARE_ATOMIC(float, max_z);
-	NNRT_INITIALIZE_ATOMIC(float, max_z, 0.f);
-
-	o3c::ParallelFor(
-			device, original_point_count,
-			NNRT_LAMBDA_CAPTURE_CLAUSE NNRT_DEVICE_WHEN_CUDACC(int64_t workload_idx) {
-				Eigen::Map<Eigen::Vector3f> point(original_point_indexer.GetDataPtr<float>(workload_idx));
-				NNRT_ATOMIC_MIN(min_x, point.x());
-				NNRT_ATOMIC_MIN(min_y, point.y());
-				NNRT_ATOMIC_MIN(min_z, point.z());
-				NNRT_ATOMIC_MAX(max_x, point.x());
-				NNRT_ATOMIC_MAX(max_y, point.y());
-				NNRT_ATOMIC_MAX(max_z, point.z());
-			}
-	);
-
-	Eigen::Vector3f grid_bin_min_bound(NNRT_GET_ATOMIC_VALUE_CPU(min_x), NNRT_GET_ATOMIC_VALUE_CPU(min_y), NNRT_GET_ATOMIC_VALUE_CPU(min_z));
-	Eigen::Vector3f grid_bin_max_bound(NNRT_GET_ATOMIC_VALUE_CPU(max_x), NNRT_GET_ATOMIC_VALUE_CPU(max_y), NNRT_GET_ATOMIC_VALUE_CPU(max_z));
-
-	NNRT_CLEAN_UP_ATOMIC(min_x);NNRT_CLEAN_UP_ATOMIC(min_y);NNRT_CLEAN_UP_ATOMIC(min_z);NNRT_CLEAN_UP_ATOMIC(max_x);NNRT_CLEAN_UP_ATOMIC(
-			max_y);NNRT_CLEAN_UP_ATOMIC(max_z);
-	//endregion
+	Eigen::Vector3f grid_bin_min_bound, grid_bin_max_bound;
+	FindPointCollectionBounds<DeviceType>(grid_bin_min_bound, grid_bin_max_bound, original_point_indexer, device, original_point_count);
 
 	// region ESTIMATE BIN COUNT IN DENSE 3D BIN BLOCK & FIND CENTER
 
@@ -278,72 +360,18 @@ void GridDownsamplePoints(open3d::core::Tensor& downsampled_points, const open3d
 	// INITIALIZE BINS
 	o3c::Blob bin_blob(static_cast<int64_t>(sizeof(PointAggregationBin) * bin_count), device);
 	auto bins = reinterpret_cast<PointAggregationBin*>(bin_blob.GetDataPtr());
-	o3c::ParallelFor(
-			device, bin_count,
-			NNRT_LAMBDA_CAPTURE_CLAUSE NNRT_DEVICE_WHEN_CUDACC(int64_t workload_idx) {
-				PointAggregationBin& bin = bins[workload_idx];
-				bin.x = 0.f;
-				bin.y = 0.f;
-				bin.z = 0.f;
-				bin.count = 0;
-			}
-	);
 
+	InitializeBins<DeviceType>(device, bin_count, bins);
+	ComputeBinAggregates<DeviceType>(original_point_indexer, device, original_point_count, bins,
+	                                 grid_bin_min_bound, grid_bin_extents, bin_size);
 
-	// COMPUTE POINT AGGREGATES FOR EACH BIN
-	o3c::ParallelFor(
-			device, original_point_count,
-			NNRT_LAMBDA_CAPTURE_CLAUSE NNRT_DEVICE_WHEN_CUDACC(int64_t workload_idx) {
-				Eigen::Map<Eigen::Vector3f> point(original_point_indexer.GetDataPtr<float>(workload_idx));
-				auto bin_coordinate = DeterminePointsBinCoordinate<DeviceType>(point, grid_bin_min_bound, bin_size);
-				auto bin_index = RavelBinLinearIndex<DeviceType>(bin_coordinate, grid_bin_extents);
-				PointAggregationBin& bin = bins[bin_index];
+	int64_t downsampled_point_count;
+	ComputeBinAverages<DeviceType>(downsampled_point_count, device, bin_count, bins);
 
-#ifdef __CUDACC__
-				atomicAdd(&bin.x, point.x());
-				atomicAdd(&bin.y, point.y());
-				atomicAdd(&bin.z, point.z());
-				atomicAdd(&bin.count, 1);
-#else
-				atomicAdd_CPU(bin.x, point.x());
-				atomicAdd_CPU(bin.y, point.y());
-				atomicAdd_CPU(bin.z, point.z());
-				bin.count++;
-#endif
-			}
-	);
-
-	NNRT_DECLARE_ATOMIC(uint32_t , downsampled_point_count_atomic);
-	NNRT_INITIALIZE_ATOMIC(uint32_t, downsampled_point_count_atomic, 0);
-
-	// COMPUTE BIN AVERAGES AND COUNT RESULTING POINTS
-	o3c::ParallelFor(
-			device, bin_count,
-			NNRT_LAMBDA_CAPTURE_CLAUSE NNRT_DEVICE_WHEN_CUDACC(int64_t workload_idx) {
-				PointAggregationBin& bin = bins[workload_idx];
-#ifdef __CUDACC__
-				if(bin.count != 0){
-					bin.x /= static_cast<float>(bin.count);
-					bin.y /= static_cast<float>(bin.count);
-					bin.z /= static_cast<float>(bin.count);
-					NNRT_ATOMIC_ADD(downsampled_point_count_atomic, 1L);
-				}
-#else
-				int bin_point_count = bin.count.load();
-				if(bin_point_count != 0){
-					bin.x.store(bin.x.load() / static_cast<float>(bin_point_count));
-					bin.y.store(bin.y.load() / static_cast<float>(bin_point_count));
-					bin.z.store(bin.z.load() / static_cast<float>(bin_point_count));
-					downsampled_point_count_atomic++;
-				}
-#endif
-			}
-	);
-
-	int64_t downsampled_point_count = static_cast<int64_t>(NNRT_GET_ATOMIC_VALUE_CPU(downsampled_point_count_atomic));
 	downsampled_points = o3c::Tensor({downsampled_point_count, 3}, o3c::Float32, device);
 	o3gk::NDArrayIndexer downsampled_point_indexer(downsampled_points, 1);
 
+	NNRT_DECLARE_ATOMIC(uint32_t, downsampled_point_count_atomic);
 	NNRT_INITIALIZE_ATOMIC(unsigned int, downsampled_point_count_atomic, 0);
 	// STORE POINTS IN RESULT TENSOR
 	o3c::ParallelFor(
@@ -359,23 +387,24 @@ void GridDownsamplePoints(open3d::core::Tensor& downsampled_points, const open3d
 					x = bin.x; y = bin.y; z = bin.z;
 				}
 #else
-				if(bin.count.load() != 0){
+				if (bin.count.load() != 0) {
 					has_point = true;
-					x = bin.x.load(); y = bin.y.load(); z = bin.z.load();
+					x = bin.x.load();
+					y = bin.y.load();
+					z = bin.z.load();
 				}
 #endif
-				if(!has_point)
+				if (!has_point)
 					return;
 
-				downsampled_point_index = NNRT_ATOMIC_ADD(downsampled_point_count_atomic, (uint32_t)1);
+				downsampled_point_index = NNRT_ATOMIC_ADD(downsampled_point_count_atomic, (uint32_t) 1);
 				Eigen::Map<Eigen::Vector3f> downsampled_point(downsampled_point_indexer.GetDataPtr<float>(downsampled_point_index));
 				downsampled_point.x() = x;
 				downsampled_point.y() = y;
 				downsampled_point.z() = z;
 
 			}
-	);
-	NNRT_CLEAN_UP_ATOMIC(downsampled_point_count_atomic);
+	);NNRT_CLEAN_UP_ATOMIC(downsampled_point_count_atomic);
 }
 
 
