Fix 2 bugs and add capability to use last frame node transformations as estimates for current.\n Fix op order bug in KEYFRAME_TO_CURRENT mode, anchor computation in PREVIOUS_TO_CURRENT mode, add capacity to use the node transformations computed at the last frame as initial estimates for the current frame.

alignment/deform_net.py
@@ -90,6 +90,8 @@ class DeformNet(nn.Module):
         self.gn_max_matches_eval = DeformNetParameters.gn_max_matches_eval.value
         self.gn_max_warped_points = DeformNetParameters.gn_max_warped_points.value
         self.gn_debug = DeformNetParameters.gn_debug.value
+        self.gn_initialize_transformations_from_provided_estimate = \
+            DeformNetParameters.gn_initialize_transformations_from_provided_estimate.value
 
         self.gn_remove_clusters_with_few_matches = DeformNetParameters.gn_remove_clusters_with_few_matches.value
         self.gn_min_num_correspondences_per_cluster = DeformNetParameters.gn_min_num_correspondences_per_cluster.value
@@ -126,7 +128,10 @@ class DeformNet(nn.Module):
             graph_clusters: torch.Tensor,
             pixel_anchors: torch.Tensor, pixel_weights: torch.Tensor, num_nodes_vec: torch.Tensor,
             intrinsics: torch.Tensor,
-            evaluate: bool = False, split: str = "train"
+            node_rotations_estimate: Union[torch.Tensor, None] = None,
+            node_translations_estimate: Union[torch.Tensor, None] = None,
+            evaluate: bool = False,
+            split: str = "train"
     ):
         batch_count = source.shape[0]
 
@@ -191,6 +196,7 @@ class DeformNet(nn.Module):
             bidirectional_correspondence_mask = \
                 (projection_error < DeformNetParameters.bidirectional_consistency_threshold.value)
             correspondence_mask = correspondence_mask & bidirectional_correspondence_mask
+            # noinspection PyTypeChecker
             correspondence_weights = torch.where(bidirectional_correspondence_mask, correspondence_weights,
                                                  torch.zeros_like(correspondence_weights))
 
@@ -203,11 +209,14 @@ class DeformNet(nn.Module):
         # We assume we always use 4 nearest anchors.
         assert pixel_anchors.shape[3] == 4
 
-        # TODO: try to reinitialize with existing rotations & translations, not blanks here
-        node_rotations = torch.eye(3, dtype=source.dtype, device=source.device).view(1, 1, 3, 3).repeat(batch_count,
-                                                                                                        num_nodes_total,
-                                                                                                        1, 1)
-        node_translations = torch.zeros((batch_count, num_nodes_total, 3), dtype=source.dtype, device=source.device)
+        if self.gn_initialize_transformations_from_provided_estimate and \
+                node_rotations_estimate is not None and node_translations_estimate is not None:
+            node_rotations = node_rotations_estimate
+            node_translations = node_translations_estimate
+        else:
+            node_rotations = torch.eye(3, dtype=source.dtype, device=source.device).view(1, 1, 3, 3) \
+                .repeat(batch_count, num_nodes_total, 1, 1)
+            node_translations = torch.zeros((batch_count, num_nodes_total, 3), dtype=source.dtype, device=source.device)
 
         deformations_validity = torch.zeros((batch_count, num_nodes_total), dtype=source.dtype, device=source.device)
         valid_solve = torch.zeros(batch_count, dtype=torch.uint8, device=source.device)
@@ -283,9 +292,9 @@ class DeformNet(nn.Module):
             cx = intrinsics[i_batch, 2]
             cy = intrinsics[i_batch, 3]
 
-            ###############################################################################################################
+            ############################################################################################################
             # region Filter invalid matches.
-            ###############################################################################################################
+            ############################################################################################################
             valid_correspondences_idxs = torch.where(correspondence_mask[i_batch])
 
             source_points_filtered = source_points[i_batch].permute(1, 2, 0)
@@ -391,6 +400,7 @@ class DeformNet(nn.Module):
                         print('cluster_id', cluster_id, cluster_match_weights)
 
                     if cluster_match_weights < self.gn_min_num_correspondences_per_cluster:
+                        # noinspection PyTypeChecker
                         index_equals_cluster_id = torch.where(graph_clusters_i == cluster_id)
                         transform_delta = index_equals_cluster_id[0].tolist()
                         node_ids_for_removal += transform_delta
@@ -411,8 +421,9 @@ class DeformNet(nn.Module):
                     optimized_node_count = graph_nodes_i.shape[0]
 
                     # Get mask of correspondences for which any one of their anchors is an invalid node
-                    valid_corresp_mask = torch.ones((match_count), dtype=torch.bool, device=source.device)
+                    valid_corresp_mask = torch.ones(match_count, dtype=torch.bool, device=source.device)
                     for node_id_for_removal in node_ids_for_removal:
+                        # noinspection PyArgumentList
                         valid_corresp_mask = valid_corresp_mask & torch.all(source_anchors != node_id_for_removal,
                                                                             axis=1)
 
@@ -631,6 +642,7 @@ class DeformNet(nn.Module):
 
         # Sample target points at computed pixel locations.
 
+        # noinspection PyUnresolvedReferences
         target_matches = torch.nn.functional.grid_sample(
             target_points, xy_coords_warped, mode=self._depth_sampling_mode, padding_mode='zeros', align_corners=False
         )
@@ -638,6 +650,7 @@ class DeformNet(nn.Module):
         # We filter out any boundary matches where any of the four pixels are invalid.
         # target_validity: mask for all points that are within the desired depth range
         target_point_validity = ((target_points > 0.0) & (target_points <= self.gn_max_depth)).type(torch.float32)
+        # noinspection PyUnresolvedReferences
         target_matches_validity = torch.nn.functional.grid_sample(
             target_point_validity, xy_coords_warped, mode="bilinear", padding_mode='zeros', align_corners=False
         )
@@ -652,12 +665,14 @@ class DeformNet(nn.Module):
         # Compute mask of valid correspondences
         valid_correspondences = valid_source_points & valid_target_matches
         valid_correspondence_counts = torch.sum(valid_correspondences, dim=(1, 2))
-        return valid_source_points, target_matches, valid_target_matches, valid_correspondences, valid_correspondence_counts
+        return valid_source_points, target_matches, valid_target_matches, valid_correspondences, \
+               valid_correspondence_counts
 
     def apply_mask_net(self, evaluate: bool, target_color: torch.Tensor, xy_coords_warped: torch.Tensor,
                        source: torch.Tensor, target_matches: torch.Tensor, features2: torch.Tensor,
                        batch_count: int, image_height: int, image_width: int) -> Tuple[torch.Tensor, torch.Tensor]:
         # Prepare the input of both the MaskNet and AttentionNet, if we actually use either of them
+        # noinspection PyUnresolvedReferences
         target_rgb_warped = torch.nn.functional.grid_sample(target_color, xy_coords_warped, padding_mode='zeros',
                                                             align_corners=False)
 
@@ -675,8 +690,10 @@ class DeformNet(nn.Module):
 
             # Patch-wise threshold
             elif self.patchwise_threshold_mask_predictions:
+                # noinspection PyUnresolvedReferences
                 pooled = torch.nn.functional.max_pool2d(input=mask_net_prediction, kernel_size=self.patch_size,
                                                         stride=self.patch_size)
+                # noinspection PyUnresolvedReferences
                 pooled = torch.nn.functional.interpolate(input=pooled.unsqueeze(1), size=(
                     self.alignment_image_height, self.alignment_image_width),
                                                          mode='nearest').squeeze(1)

alignment/interface.py
@@ -16,7 +16,9 @@ def run_non_rigid_alignment(deform_net: DeformNet,
                             pixel_weights: open3d.core.Tensor,
                             graph: GraphWarpField,
                             cropped_intrinsics: np.ndarray,
-                            device: open3d.core.Device) -> dict:
+                            device: open3d.core.Device,
+                            use_graph_rotations_and_translations_as_estimates=True,
+                            use_warped_nodes=False) -> dict:
     torch_device = torch.device(repr(device).lower())
 
     source_cuda = torch.from_numpy(source_rgbxyz).to(torch_device).unsqueeze(0)
@@ -24,11 +26,20 @@ def run_non_rigid_alignment(deform_net: DeformNet,
     pixel_anchors_cuda = torch.utils.dlpack.from_dlpack(pixel_anchors.to_dlpack()).unsqueeze(0)
     pixel_weights_cuda = torch.utils.dlpack.from_dlpack(pixel_weights.to_dlpack()).unsqueeze(0)
 
-    warped_nodes = graph.get_warped_nodes()
-    graph_nodes_cuda = torch.utils.dlpack.from_dlpack(warped_nodes.to_dlpack()).unsqueeze(0)
+    if use_warped_nodes:
+        graph_nodes_cuda = torch.utils.dlpack.from_dlpack(graph.get_warped_nodes().to_dlpack()).unsqueeze(0)
+    else:
+        graph_nodes_cuda = torch.utils.dlpack.from_dlpack(graph.nodes.to_dlpack()).unsqueeze(0)
     graph_edges_cuda = torch.utils.dlpack.from_dlpack(graph.edges.to_dlpack()).unsqueeze(0)
     graph_edges_weights_cuda = torch.utils.dlpack.from_dlpack(graph.edge_weights.to_dlpack()).unsqueeze(0)
     graph_clusters_cuda = torch.utils.dlpack.from_dlpack(graph.clusters.to_dlpack()).reshape(-1, 1).unsqueeze(0)
+
+    if use_graph_rotations_and_translations_as_estimates:
+        node_rotations_estimate_cuda = torch.utils.dlpack.from_dlpack(graph.rotations.to_dlpack()).unsqueeze(0)
+        node_translations_estimate_cuda = torch.utils.dlpack.from_dlpack(graph.translations.to_dlpack()).unsqueeze(0)
+    else:
+        node_rotations_estimate_cuda = None
+        node_translations_estimate_cuda = None
 
     intrinsics_cuda = torch.from_numpy(cropped_intrinsics).to(torch_device).unsqueeze(0)
 
@@ -44,6 +55,8 @@ def run_non_rigid_alignment(deform_net: DeformNet,
             graph_nodes_cuda, graph_edges_cuda, graph_edges_weights_cuda, graph_clusters_cuda,
             pixel_anchors_cuda, pixel_weights_cuda,
             node_count_cuda, intrinsics_cuda,
+            node_rotations_estimate=node_rotations_estimate_cuda,
+            node_translations_estimate=node_translations_estimate_cuda,
             evaluate=True, split="test"
         )
     return deform_net_data

apps/fusion/pipeline.py
@@ -319,12 +319,9 @@ class FusionPipeline:
                 ########################################################################################################
                 # region prepare pixel anchors, and pixel weights
                 ########################################################################################################
-                if current_frame_is_keyframe:
-                    saved_pixel_anchors, saved_pixel_weights = self.compute_pixel_anchors(source_point_image_np, device)
-
                 if tracking_parameters.tracking_span_mode.value is TrackingSpanMode.PREVIOUS_TO_CURRENT:
                     pixel_anchors, pixel_weights = \
-                        self.compute_pixel_anchors(source_point_image_np, device)
+                        self.compute_pixel_anchors(source_point_image_np, device, use_warped_nodes=True)
                 else:
                     pixel_anchors = saved_pixel_anchors
                     pixel_weights = saved_pixel_weights
@@ -345,9 +342,16 @@ class FusionPipeline:
                 # region === run the motion prediction & optimization (non-rigid alignment) ====
                 #####################################################################################################
 
-                deform_net_data = run_non_rigid_alignment(self.deform_net, source_rgbxyz, target_rgbxyz, pixel_anchors,
-                                                          pixel_weights, self.active_graph, cropped_intrinsics_numpy,
-                                                          device)
+                deform_net_data = run_non_rigid_alignment(
+                    self.deform_net, source_rgbxyz, target_rgbxyz, pixel_anchors,
+                    pixel_weights, self.active_graph, cropped_intrinsics_numpy,
+                    device,
+                    use_graph_rotations_and_translations_as_estimates=
+                    tracking_parameters.tracking_span_mode.value != TrackingSpanMode.PREVIOUS_TO_CURRENT,
+                    use_warped_nodes=
+                    tracking_parameters.tracking_span_mode.value == TrackingSpanMode.PREVIOUS_TO_CURRENT,
+                )
+
                 telemetry_generator.process_gn_point_clouds(deform_net_data["gn_point_clouds"])
                 telemetry_generator.process_correspondences(deform_net_data["correspondence_info"],
                                                             deform_net_data["mask_pred"])
@@ -381,6 +385,7 @@ class FusionPipeline:
                     self.active_graph.rotations = node_rotation_predictions
                     self.active_graph.translations = node_translation_predictions
                     if len(self.keyframe_graphs) == 0:
+                        # we're at the initial keyframe, graph storage is unnecessary
                         graph_for_integration = self.active_graph
                     else:
                         graph_for_integration = self.keyframe_graphs[-1].clone()
@@ -390,6 +395,9 @@ class FusionPipeline:
                     if current_frame_is_keyframe:
                         self.active_graph = graph_for_integration.apply_transformations()
                         self.keyframe_graphs.append(graph_for_integration)
+                        # have to recompute the pixel anchors and weights with the new source point cloud and graph
+                        saved_pixel_anchors, saved_pixel_weights = \
+                            self.compute_pixel_anchors(source_point_image_np, device, use_warped_nodes=False)
 
                 # handle logging/vis of the graph data
                 telemetry_generator.process_graph_transformation(graph_for_integration)
@@ -432,6 +440,7 @@ class FusionPipeline:
             end_time = timeit.default_timer()
             print(f"Total runtime (in seconds, with graph generation, "
                   f"without model + TSDF grid initialization): {end_time - start_time}")
+
         return PROGRAM_EXIT_SUCCESS
 
     def initialize_graph_and_anchors(self, volume: nnrt.geometry.WarpableTSDFVoxelGrid,
@@ -486,28 +495,34 @@ class FusionPipeline:
             depth_scale = deform_net_parameters.depth_scale.value
             source_point_image = image_processing.backproject_depth(depth_image_np, self.fx, self.fy, self.cx, self.cy,
                                                                     depth_scale=depth_scale)  # (h, w, 3)
-            return self.compute_pixel_anchors(source_point_image, device)
+            return self.compute_pixel_anchors(source_point_image, device, use_warped_nodes=False)
 
         precomputed_pixel_anchors = self.cropper(precomputed_pixel_anchors)
         precomputed_pixel_weights = self.cropper(precomputed_pixel_weights)
-        return o3c.Tensor(precomputed_pixel_anchors, device=device), o3c.Tensor(precomputed_pixel_weights,
-                                                                                device=device)
+        return o3c.Tensor(precomputed_pixel_anchors, device=device), \
+               o3c.Tensor(precomputed_pixel_weights, device=device)
 
-    def compute_pixel_anchors(self, source_point_image: np.ndarray, device: o3c.Device) \
+    def compute_pixel_anchors(self, source_point_image: np.ndarray, device: o3c.Device,
+                              use_warped_nodes=False) \
             -> Tuple[o3c.Tensor, o3c.Tensor]:
         tracking_parameters = Parameters.fusion.tracking
         node_coverage = Parameters.graph.node_coverage.value
         source_point_image_o3d = o3c.Tensor(source_point_image, device=device)
+        if use_warped_nodes:
+            nodes = self.active_graph.get_warped_nodes()
+        else:
+            nodes = self.active_graph.nodes
 
         if tracking_parameters.pixel_anchor_computation_mode.value == AnchorComputationMode.EUCLIDEAN:
             pixel_anchors, pixel_weights = \
                 nnrt.geometry.compute_anchors_and_weights_euclidean(
-                    source_point_image_o3d, self.active_graph.nodes, 4, 0, node_coverage
+                    source_point_image_o3d, nodes, 4, 0, node_coverage
                 )
         elif tracking_parameters.pixel_anchor_computation_mode.value == AnchorComputationMode.SHORTEST_PATH:
             pixel_anchors, pixel_weights = \
                 nnrt.geometry.compute_anchors_and_weights_shortest_path(
-                    source_point_image_o3d, self.active_graph.nodes, self.active_graph.edges, 4, node_coverage
+                    source_point_image_o3d, nodes, self.active_graph.edges, 4,
+                    node_coverage
                 )
         else:
             raise NotImplementedError(